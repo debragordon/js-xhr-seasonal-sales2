f03422eba334f52f6434df225571c3ab0bee5929
Debra Gordon <debra.m.gordon@gmail.com>
2016-09-14 15:02:12 -0500

    got a little further in the main.js file

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,11 @@
 var products = [];
-var categories = [];
+var discounts = [];
 
 //loads the products at full price
 var productLoad = new XMLHttpRequest();
 productLoad.open("GET", "data/products.json");
 productLoad.send();
 productLoad.addEventListener("load", function () {
-
   var data = JSON.parse(this.responseText);
   products = data.products;
   var productContainer = document.getElementById("storefront");
@@ -15,4 +14,51 @@ productLoad.addEventListener("load", function () {
   }
 });
 
-//calls the discounts
+var seasons = [];
+var departments = [];
+var discountID = [];
+//grabs the seasons and departments from the JSON file
+var discountGetter = new XMLHttpRequest();
+discountGetter.open("GET", "data/categories.json");
+discountGetter.send();
+discountGetter.addEventListener("load", function () {
+  var data = JSON.parse(this.responseText);
+  discounts = data.categories;
+  for (var i = 0; i < discounts.length; i++) {
+    seasons.push(discounts[i].season_discount);
+    departments.push(discounts[i].name);
+    discountID.push(discounts[i].id);
+  }
+  console.log(seasons);
+  console.log(departments);
+  console.log(discountID);
+});
+
+//match deals with products and set to the empty variables (arrays) below
+var winterDeals = [];
+var autumnDeals = [];
+var springDeals = [];
+
+///////////////// I am stuck here /////////////////
+
+//click events to dynamically display the correct deals ... including swapping out the price
+function listClicker(){
+  var e = document.getElementById("discount-selector");
+    if (e.selectedIndex === "autumn") {
+      //display autumnDeals in the DOM
+      alert("you picked autumn");
+    }
+    if (e.selectedIndex === "winter") {
+      //display winterDeals in the DOM
+      alert("you picked winter");
+    }
+    if (e.selectedIndex === "spring") {
+      //display springDeals in the DOM
+      alert("you picked spring");
+    }
+  }
+
+document.getElementById("discount-selector").addEventListener("click", listClicker);
+
+
+
