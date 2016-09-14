073b9b34f46a8cd6ccbf78470a31684a4be33f4c
Debra Gordon <debra.m.gordon@gmail.com>
2016-09-14 14:02:58 -0500

    working on initial display

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,18 @@
 var products = [];
 var categories = [];
 
-//loads the products
+//loads the products at full price
 var productLoad = new XMLHttpRequest();
 productLoad.open("GET", "data/products.json");
 productLoad.send();
 productLoad.addEventListener("load", function () {
 
   var data = JSON.parse(this.responseText);
-  // console.log(data);
   products = data.products;
-  // console.log(products);
   var productContainer = document.getElementById("storefront");
-
   for (var i = 0; i < products.length; i++) {
-    productContainer.innerHTML += `<div class='product'><img src="${products[i].jpg}" alt="" class="block"><div class="inline">${products[i].name} $${products[i].price}</div></div>`;
+    productContainer.innerHTML += `<div class='product'><img src="${products[i].jpg}" alt="" class="block"><div class="inline">${products[i].name} <p class="currentPrice">Current Price $${products[i].reg_price}</p></div></div>`;
   }
-
-
 });
+
+//calls the discounts
